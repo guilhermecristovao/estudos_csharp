7ff45ed20b3cd7a2a309a4c9fbfe12412c556706
Guilherme Vinícius Rosa Cristóvão <37309998+guilhermecristovao@users.noreply.github.com>
2019-04-24 12:26:42 -0300

    Add files via upload

---
+++ aula019.cs
@@ -0,0 +1,36 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: pontonet
+ * Date: 24/04/19
+ * Time: 12:19
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace aula020
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			//Conectivos lógicos
+			int num = 0;
+			Console.WriteLine("Informe um numero inteiro de 20 a 30.");
+			num = Convert.ToInt32(Console.ReadLine());
+			
+			if(num >= 20 && num < 30)
+			{
+				Console.WriteLine("O número está no intervalo.");
+			}
+			else
+			{
+				Console.WriteLine("Número incorreto!");
+			}
+			
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
